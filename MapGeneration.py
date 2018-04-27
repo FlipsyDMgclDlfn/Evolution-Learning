344d862db5044ba67e5b75749810a8e63ca0ae3a
FlipsyDMgclDlfn <34141795+FlipsyDMgclDlfn@users.noreply.github.com>
2018-04-27 15:36:58 -0400

    works
    jk

--- MapGeneration.py
+++ MapGeneration.py
@@ -1,28 +1,28 @@
 import pygame
+import time
 from random import *
 
 pygame.init()
-display = pygame.display.set_mode((800,600))
-display.fill((255,0,255))
+display = pygame.display.set_mode((600,600))
+display.fill((255,255,255))
+clock = pygame.time.Clock()
 
-
-class Tile:    
+class Tile:
     def __init__(self,x,y):
+        self.x = x
+        self.y = y
         foodChance = 30
         maxFood = 20
         minFood = 10
-        if randint(0,100) < foodChance: self.food = randint(minFood,maxFood)
-        else: self.food = 0
-        self.x = x
-        self.y = y
+        r=randint(0,100)
+        if r < foodChance:
+            self.food = int(randint(minFood,maxFood))
+        else:
+            self.food = int(0)
         
-class Land(Tile):
-    def __init__(self,x,y):
-        Tile.__init__(self,x,y)
-        
-class Water(Tile):
-    def __init__(self,x,y):
-        Tile.__init__(self,x,y)
+    def draw(self):
+        if int(self.food) != int(0):
+            pygame.draw.rect( display, (0,200,0),( (self.x)*(20), (self.y)*(20), (self.x+1)*(20), (self.y+1)*(20) ) )
 
 
 class Map:
@@ -33,18 +33,20 @@ class Map:
         for i in range(width):
             self.board+=[[]]
             for j in range(height):
-                if randint(0,3) != 0: self.board[i] += [Land(i,j)]
-                else: self.board[i] += [Water(i,j)]
+                self.board[i] += [Tile(i,j)]
     def draw(self):
-        for i in range(len(self.board)):
-            for j in range(len(self.board[i])):
-                if self.board[i][j] is Land:
-                    pygame.draw.rect(display, (0,200,0),(i*(800/self.width),j*(600/self.width),(i+1)*(800/self.width),(j+1)*(600/self.width)))
+        for i in range(self.width):
+            for j in range(self.height):
+                self.board[i][j].draw()
 
-ma = Map(40,30)
+ma = Map(30,30)
+ma.draw()
+print(a,b)
 carryOn = True
 while carryOn:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
               carryOn = False
-    ma.draw()
+
+    pygame.display.update()
+    clock.tick(30)
