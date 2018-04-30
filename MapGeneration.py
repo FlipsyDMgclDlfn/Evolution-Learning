7eda84cfb57d761674a172752cb8530580fc43dc
FlipsyDMgclDlfn <34141795+FlipsyDMgclDlfn@users.noreply.github.com>
2018-04-30 07:22:15 -0400

    display actually works!

--- MapGeneration.py
+++ MapGeneration.py
@@ -3,7 +3,9 @@ import time
 from random import *
 
 pygame.init()
-display = pygame.display.set_mode((600,600))
+WIDTH = 600
+HEIGHT = 600
+display = pygame.display.set_mode((WIDTH,HEIGHT))
 display.fill((255,255,255))
 clock = pygame.time.Clock()
 
@@ -14,34 +16,35 @@ class Tile:
         foodChance = 30
         maxFood = 20
         minFood = 10
-        r=randint(0,100)
+        r=randint(0,100)        
         if r < foodChance:
-            self.food = int(randint(minFood,maxFood))
+            self.food = randint(minFood,maxFood)
         else:
-            self.food = int(0)
-        
-    def draw(self):
-        if int(self.food) != int(0):
-            pygame.draw.rect( display, (0,200,0),( (self.x)*(20), (self.y)*(20), (self.x+1)*(20), (self.y+1)*(20) ) )
-
+            self.food = 0
+            
+    def draw(self, xsize, ysize):
+        if self.food != 0:
+            pygame.draw.rect( display, (0,200,0),( (self.x)*(xsize), (self.y)*(ysize), xsize, ysize ) )
+        else:
+            pygame.draw.rect( display, (222,184,135),( (self.x)*(xsize), (self.y)*(ysize), xsize, ysize ) )
 
 class Map:
     def __init__(self,width,height):
-        self.width=width
-        self.height=height
-        self.board=[]
-        for i in range(width):
-            self.board+=[[]]
-            for j in range(height):
-                self.board[i] += [Tile(i,j)]
+        self.width = width
+        self.height = height
+        self.board = []
+        for i in range(height):
+            self.board += [[]]
+            for j in range(width):
+                self.board[i] += [Tile(j,i)]
     def draw(self):
-        for i in range(self.width):
-            for j in range(self.height):
-                self.board[i][j].draw()
+        for i in range(self.height):
+            for j in range(self.width):
+                self.board[i][j].draw(WIDTH/self.width,HEIGHT/self.height)
 
-ma = Map(30,30)
+ma = Map(120,120)
 ma.draw()
-print(a,b)
+
 carryOn = True
 while carryOn:
     for event in pygame.event.get():
